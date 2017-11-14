f1c89a76ca3b748507f46199cb968a112dfdcb75
Patricia Au <patau@uw.edu>
2017-11-14 13:52:50 -0800

    Function added to scripts/

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+
+f <- function(data, x, y, color, title, xl, yl) {
+  attach(data)
+  plot(x, y, main=title, 
+       xlab=xl, ylab=yl)
+}
